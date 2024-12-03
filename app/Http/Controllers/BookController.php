5c1fecef7e2eecf9b12e0a8ef6a47e2b2d08ce7f
AlhikamWarsawa <alhikam200@gmail.com>
2024-12-03 11:07:05 +0700

    Update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,7 @@ class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::latest()->paginate(2);
+        $books = Book::latest()->paginate(5);
         return view('books.index', compact('books'));
     }
 
